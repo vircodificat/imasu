064e53a3a5ced1a6d06a6ef2bd8a05ffb367b925
Alice Ziuziakowska <a@saccharin.moe>
2024-12-20 17:58:44 +0000

    initial commit

---
+++ build.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const root = b.path("src/imasu.zig");
+
+    const exe = b.addExecutable(.{
+        .name = "imasu",
+        .root_source_file = root,
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the emulator");
+    run_step.dependOn(&run_cmd.step);
+}
