41ff512f4f81befc1df9b8b12b7a494005eb6c50
Alice Ziuziakowska <a@saccharin.moe>
2025-01-06 04:45:58 +0000

    create MMIO Device interface
    Memory-mapped I/O devices implement `mmio_reg_read` and
    =mmio_reg_write`, which are called when the device is accessed through
    `Memory` if the access is within the mmio region of the device.
    `run` is called by the main loop of the emulator for the device to
    execute its logic to do processing or interrupt the hart

---
+++ src/device.zig
@@ -0,0 +1,31 @@
+// MMIO-accessible devices
+
+const std = @import("std");
+
+const Device = @This();
+
+kind: union(enum) {}, // device type
+
+mmio_base: u64, // start of device mmio region
+mmio_len: u64, // length of device mmio region
+
+// read naturally-aligned power-of-two bytes from device register reg_addr
+pub fn mmio_reg_read(dev: *Device, comptime T: type, reg_addr: u64) !T {
+    return switch (dev.kind) {
+        inline else => |impl| impl.mmio_reg_read(T, reg_addr),
+    };
+}
+
+// write naturally-aligned power-of-two bytes to device register reg_addr
+pub fn mmio_reg_write(dev: *Device, comptime T: type, reg_addr: u64, v: T) !void {
+    return switch (dev.kind) {
+        inline else => |impl| impl.mmio_reg_write(T, reg_addr, v),
+    };
+}
+
+// run the device
+pub fn run(dev: *Device) void {
+    switch (dev.kind) {
+        inline else => |impl| impl.run(),
+    }
+}
