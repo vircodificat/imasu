f0356e626cc7a098109bfc33517068ef35c01532
Alice Ziuziakowska <a@saccharin.moe>
2025-01-30 22:09:10 +0000

    clint: make `mtime` read-only
    there isn't much of a reason to have this be writeable, as linux does
    not write to it

--- src/devices/clint.zig
+++ src/devices/clint.zig
@@ -16,7 +16,7 @@ timer: Timer,
 interrupt_target: *Hart, // hart that this CLINT interrupts
 
 const reg_mswi = 0x0; // 4 bytes, lowest bit reads/writes target hart's msip bit
-const reg_mtime = 0xbff8; // 8 bytes, reads/writes value of the timer
+const reg_mtime = 0xbff8; // 8 bytes, reads value of the timer
 const reg_mtimecmp = 0x4000; // 8 bytes, reads/writes comparion value of the timer
 
 pub const mmio_len = 0xc000;
@@ -32,10 +32,7 @@ pub fn mmio_reg_read(clint: *CLINT, comptime T: type, reg_addr: u64) !T {
             else => return Exception.LoadAccessFault,
         },
         u32 => switch (reg_addr) { // 4-byte access to all registers
-            reg_mswi => {
-                const msip = clint.interrupt_target.csrs.mip.msip;
-                return @as(T, @intFromBool(msip));
-            },
+            reg_mswi => return @as(T, @intFromBool(clint.interrupt_target.csrs.mip.msip)),
             reg_mtime => return @truncate(clint.mtime),
             reg_mtime + 4 => return @truncate(clint.mtime >> 32),
             reg_mtimecmp => return @truncate(clint.mtimecmp),
@@ -49,36 +46,14 @@ pub fn mmio_reg_read(clint: *CLINT, comptime T: type, reg_addr: u64) !T {
 pub fn mmio_reg_write(clint: *CLINT, comptime T: type, reg_addr: u64, v: T) !void {
     switch (T) {
         u64 => switch (reg_addr) {
-            reg_mtime => {
-                clint.mtime = v;
-                clint.timer_check();
-            },
-            reg_mtimecmp => {
-                clint.mtimecmp = v;
-                clint.timer_check();
-            },
+            reg_mtimecmp => clint.mtimecmp = v,
             else => return Exception.StoreAccessFault,
         },
         u32 => switch (reg_addr) {
-            reg_mswi => {
-                clint.interrupt_target.assert_interrupt_pending(.Software, v & 0b1 == 0b1);
-            },
-            reg_mtime => {
-                clint.mtime = (clint.mtime & mask_hi) | v;
-                clint.timer_check();
-            },
-            reg_mtime + 4 => {
-                clint.mtime = (clint.mtime & mask_lo) | @as(u64, v) << 32;
-                clint.timer_check();
-            },
-            reg_mtimecmp => {
-                clint.mtimecmp = (clint.mtimecmp & mask_hi) | v;
-                clint.timer_check();
-            },
-            reg_mtimecmp + 4 => {
-                clint.mtimecmp = (clint.mtimecmp & mask_lo) | @as(u64, v) << 32;
-                clint.timer_check();
-            },
+            reg_mswi => clint.interrupt_target.set_interrupt_pending(.Software, v & 0b1 == 0b1),
+            reg_mtime, reg_mtime + 4 => return Exception.StoreAccessFault,
+            reg_mtimecmp => clint.mtimecmp = (clint.mtimecmp & mask_hi) | v,
+            reg_mtimecmp + 4 => clint.mtimecmp = (clint.mtimecmp & mask_lo) | @as(u64, v) << 32,
             else => return Exception.StoreAccessFault,
         },
         else => return Exception.StoreAccessFault,
